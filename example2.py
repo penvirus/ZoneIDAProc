ab936342783171d4e11f777c62cbba059375b184
Tzung-Bi Shih <penvirus@gmail.com>
2015-03-17 04:37:14 +0800

    fix reference error

--- example2.py
+++ example2.py
@@ -21,7 +21,7 @@ def instrument_code(pid, filename):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print 'Usage: %s [pid]' % argv[0]
+        print 'Usage: %s [pid]' % sys.argv[0]
         sys.exit(1)
 
     pid = int(sys.argv[1])
