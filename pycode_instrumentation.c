a2a18b81f3d62e9f5de689aac0fa3ab5dd4f2db1
Tzung-Bi Shih <penvirus@gmail.com>
2015-03-16 19:59:37 +0800

    simplestring and simplefile

--- pycode_instrumentation.c
+++ pycode_instrumentation.c
@@ -2,45 +2,70 @@
 #include <dlfcn.h>
 #include <stdio.h>
 
-int instrument(const char *command)
+static int (*_Py_IsInitialized)() = NULL;
+static PyInterpreterState *(*_PyInterpreterState_Head)() = NULL;
+static PyGILState_STATE (*_PyGILState_Ensure)() = NULL;
+static void (*_PyGILState_Release)(PyGILState_STATE) = NULL;
+static int (*_PyRun_SimpleString)(const char *) = NULL;
+static int (*_PyRun_SimpleFile)(FILE *, const char *) = NULL;
+
+static int _call_dlsym()
 {
-	void *main_hndl = dlopen(NULL, 0x2);
+	void *main_handle = dlopen(NULL, 0x2);
 
 	//int Py_IsInitialized()
-	int (*_Py_IsInitialized)() = (int (*)()) dlsym(main_hndl, "Py_IsInitialized");
+	_Py_IsInitialized = (int (*)()) dlsym(main_handle, "Py_IsInitialized");
 	if(_Py_IsInitialized == NULL) {
 		printf("dlsym Py_IsInitialized is NULL\n");
 		goto error;
 	}
 
 	//PyInterpreterState *PyInterpreterState_Head()
-	PyInterpreterState *(*_PyInterpreterState_Head)() = (PyInterpreterState* (*)()) dlsym(main_hndl, "PyInterpreterState_Head");
+	_PyInterpreterState_Head = (PyInterpreterState* (*)()) dlsym(main_handle, "PyInterpreterState_Head");
 	if(_PyInterpreterState_Head == NULL) {
 		printf("dlsym PyInterpreterState_Head is NULL\n");
 		goto error;
 	}
 
 	//PyGILState_STATE PyGILState_Ensure()
-	PyGILState_STATE (*_PyGILState_Ensure)() = (PyGILState_STATE (*)()) dlsym(main_hndl, "PyGILState_Ensure");
+	_PyGILState_Ensure = (PyGILState_STATE (*)()) dlsym(main_handle, "PyGILState_Ensure");
 	if(_PyGILState_Ensure == NULL) {
 		printf("dlsym PyGILState_Ensure is NULL\n");
 		goto error;
 	}
 
 	//void PyGILState_Release(PyGILState_STATE oldstate)
-	void (*_PyGILState_Release)(PyGILState_STATE) = (void (*)(PyGILState_STATE)) dlsym(main_hndl, "PyGILState_Release");
+	_PyGILState_Release = (void (*)(PyGILState_STATE)) dlsym(main_handle, "PyGILState_Release");
 	if(_PyGILState_Release == NULL) {
 		printf("dlsym PyGILState_Release is NULL\n");
 		goto error;
 	}
 
 	//int PyRun_SimpleString(const char *command)
-	int (*_PyRun_SimpleString)(const char*) = (int (*)(const char *)) dlsym(main_hndl, "PyRun_SimpleString");
+	_PyRun_SimpleString = (int (*)(const char *)) dlsym(main_handle, "PyRun_SimpleString");
 	if(_PyRun_SimpleString == NULL) {
 		printf("dlsym PyRun_SimpleString is NULL\n");
 		goto error;
 	}
 
+	//int PyRun_SimpleFile(FILE *fp, const char *filename)
+	_PyRun_SimpleFile = (int (*)(FILE *, const char *)) dlsym(main_handle, "PyRun_SimpleFile");
+	if(_PyRun_SimpleFile == NULL) {
+		printf("dlsym PyRun_SimpleFile is NULL\n");
+		goto error;
+	}
+
+	return 1;
+error:
+	return 0;
+}
+
+int instrument_string(const char *command)
+{
+	if(!_call_dlsym()) {
+		printf("_call_dlsym is failed");
+		goto error;
+	}
 
 	if(!_Py_IsInitialized()){
 		printf("Py_IsInitialized returned false.\n");
@@ -59,5 +84,43 @@ int instrument(const char *command)
 
 	return 1;
 error:
+	return 0;
+}
+
+int instrument_file(const char *filename)
+{
+	FILE *fp = NULL;
+
+	if(!_call_dlsym()) {
+		printf("_call_dlsym is failed");
+		goto error;
+	}
+
+	if(!_Py_IsInitialized()){
+		printf("Py_IsInitialized returned false.\n");
+		goto error;
+	}
+
+	PyInterpreterState *head = _PyInterpreterState_Head();
+	if(head == NULL) {
+		printf("Interpreter is not initialized\n");
+		goto error;
+	}
+
+	PyGILState_STATE pyGILState = _PyGILState_Ensure();
+	fp = fopen(filename, "r");
+	if(fp == NULL) {
+		printf("file %s doesn't exist", filename);
+		goto error;
+	}
+	_PyRun_SimpleFile(fp, "Instrumentation");
+	_PyGILState_Release(pyGILState);
+
+	if(fp)
+		fclose(fp);
+	return 1;
+error:
+	if(fp)
+		fclose(fp);
 	return 0;
 }
