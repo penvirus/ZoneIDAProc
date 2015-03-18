b17c5bafed0e2a74409e1a8fe19a94b54065f2cb
Tzung-Bi Shih <penvirus@gmail.com>
2015-03-18 13:01:57 +0800

    example1 ctime and closure data

--- example1.py
+++ example1.py
@@ -1,23 +1,27 @@
+import time
 from ida_proc import IDAProc
 
 app = IDAProc()
 
-@app.route('test/file_a')
-def file_a():
-    return 'Test String'
+@app.route('/time')
+def ctime():
+    return time.ctime()
 
-b_data = 99999
+def register_for_data():
+    # nonlocal in python 2.7
+    # https://technotroph.wordpress.com/2012/10/01/python-closures-and-the-python-2-7-nonlocal-solution/
+    data = dict()
+    data['data'] = 99999
 
-@app.route('test/file_b')
-def file_b():
-    global b_data
-    return b_data
+    @app.route('/test/data')
+    def getter():
+        return data['data']
 
-@app.route('test/file_b', method='SET')
-def file_b(data):
-    global b_data
-    b_data = data
-    return data
+    @app.route('/test/data', method='SET')
+    def setter(d):
+        data['data'] = d
+        return data['data']
 
 if __name__ == '__main__':
+    register_for_data()
     app.run()
